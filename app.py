736be3802ee0f67f75ed8e5fe6908eea1bbe754b
Mike Kevin De Vera <117539588+Reccamike23@users.noreply.github.com>
2023-12-11 19:28:38 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+# Import necessary libraries
+import streamlit as st
+from gradientai import Gradient
+
+def main():
+    # Streamlit title and description
+    st.title("Interactive Food Drive Assistant")
+    st.write("Ask a question about the Food Drive!")
+
+    with Gradient() as gradient:
+        base_model = gradient.get_base_model(base_model_slug="nous-hermes2")
+        new_model_adapter = base_model.create_model_adapter(name="interactive_food_drive_model")
+
+        user_input = st.text_input("Ask your question:")
+        if user_input and user_input.lower() not in ['quit', 'exit']:
+            sample_query = f"### Instruction: {user_input} \n\n### Response:"
+            st.markdown(f"Asking: {sample_query}")
+
+            # before fine-tuning
+            completion = new_model_adapter.complete(query=sample_query, max_generated_token_count=100).generated_output
+            st.markdown(f"Generated: {completion}")
+
+        # Delete the model adapter after generating the response
+        new_model_adapter.delete()
+
+if __name__ == "__main__":
+    main()
